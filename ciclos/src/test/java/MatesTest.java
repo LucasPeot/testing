c36397c76a3e27acbb9fd7cb867c0f345fc84971
barqueiro18 <lucaspeot@gmail.com>
2026-05-11 12:14:05 +0000

    cambios

---
+++ ciclos/src/test/java/MatesTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com..Mates;
+
+public class MatesTest {
+     @Test
+     public void testDividir(){
+        int resultado = con.contornos.Mates.dividirStatic(20, 5);
+        assertEquals(4,resultado);
+     }
+}
